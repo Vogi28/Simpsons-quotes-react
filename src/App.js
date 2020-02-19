5226d17ffb3100672adb49f83bfb63c9cd568ee2
NGUYEN Vo Vinh Nghi <vogi.nguyen@live.fr>
2020-02-19 11:24:43 +0100

    list of quotes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import QuoteCard from './components/QuoteCard';
-
-const infos = {
-    character : "Homer Simpson",
-    image : "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939",
-    quotes : "I hope I didn't brain my damage.",
-};
+import QuoteList from './components/QuoteList';  
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <QuoteCard character={infos.character} image={infos.image} quote={infos.quotes} />
+      <QuoteList />
     </div>
   );
 }
