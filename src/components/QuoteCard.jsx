47334c130bbfa2a6edd333af68630c9b0d131682
NGUYEN Vo Vinh Nghi <vogi.nguyen@live.fr>
2020-02-20 17:37:45 +0100

    react state

--- src/components/QuoteCard.jsx
+++ src/components/QuoteCard.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
+import './QuoteCard.css';
 
-function QuoteCard(props) {
+class QuoteCard extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorite : false
+    };
+  }
+
+  handleClick = (e) => {
+    this.setState({favorite : !this.state.favorite});
+    console.log(this.state.favorite);
+  }
+
+  render () {
     return (
+      <div>
         <figure className="QuoteCard">
-       <img src={props.image} alt={props.character}/>
+       <img src={this.props.image} alt={this.props.character}/>
     <figcaption>
         <blockquote>
-            {props.quote}
+            {this.props.quote}
         </blockquote>
-      <cite>{props.character}</cite>
+        <p>
+          <cite>{this.props.character}</cite>
+          <span className={this.state.favorite ? 'is-favorite' : ''} onClick={this.handleClick}>&#9733;</span>
+        </p>
     </figcaption>
   </figure>
+  </div>
     );
+  }
 }
 
 export default QuoteCard;
